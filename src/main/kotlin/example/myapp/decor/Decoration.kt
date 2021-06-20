bd289405778427f86652cfe89bc9ddd825b59ce3
Dagmar Cirino <dagmar.mv@gmail.com>
2021-06-19 23:23:07 -0300

    revisão de decorators

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -1,7 +1,6 @@
 package example.myapp.decor
 
-data class Decoration(val rocks: String) {
-}
+data class Decoration(val rocks: String)
 
 enum class Color(val rgb: Int) {
     RED(0xFF0000),
@@ -31,11 +30,11 @@ fun makeDecorations() {
     println(data3.equals(data2))
 }
 
+
 data class Decorations2(
     val rocks: String,
     val wood: String,
-    val diver: String){
-}
+    val diver: String)
 
 fun makeDecorations2() {
     val data4 = Decorations2(
