324b4a4b527eeabf51e3ef7fa6d4a054194e3d48
Dagmar Cirino <dagmar.mv@gmail.com>
2021-06-20 22:04:54 -0300

    avançando com generic types

--- src/main/kotlin/example/myapp/generics/Aquarium.kt
+++ src/main/kotlin/example/myapp/generics/Aquarium.kt
@@ -16,21 +16,39 @@ class LakeWater: WaterSupply(true){
     }
 }
 
-class Aquarium<T: WaterSupply>(val waterSupply: T) {
-    fun addWater() {
-        check(!waterSupply.needsProcessing) {
-            "water supply needs processing first"
+
+interface Cleaner<in T: WaterSupply> {
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner: Cleaner<TapWater> {
+    override fun clean(waterSupply: TapWater) {
+        waterSupply.addChemicalCleaners()
+        println("Aquario limpo")
+    }
+}
+
+class Aquarium<out T: WaterSupply>(val waterSupply: T) {
+    fun addWater(cleaner: Cleaner<T>) {
+        if (waterSupply.needsProcessing) {
+            cleaner.clean(waterSupply)
         }
-        println("adding water from $waterSupply")
+
+        println("água adicionada")
+
+//        check(!waterSupply.needsProcessing) {
+//            "water supply needs processing first"
+//        }
+//        println("adding water from $waterSupply")
     }
 }
 
 
 fun genericsExample() {
-    val aquarium = Aquarium(TapWater())
-    println("water needs processing: ${aquarium.waterSupply.needsProcessing}")
-    aquarium.waterSupply.addChemicalCleaners()
-    println("water needs processing: ${aquarium.waterSupply.needsProcessing}")
+//    val aquarium = Aquarium(TapWater())
+//    println("water needs processing: ${aquarium.waterSupply.needsProcessing}")
+//    aquarium.waterSupply.addChemicalCleaners()
+//    println("water needs processing: ${aquarium.waterSupply.needsProcessing}")
 
 //    val aquarium2 = Aquarium("string")
 //    println(aquarium2.waterSupply)
@@ -43,11 +61,31 @@ fun genericsExample() {
 //    val aquarium4 = Aquarium("string")
 //    println(aquarium4.waterSupply)
 
-    val aquarium5 = Aquarium(LakeWater())
-    aquarium5.waterSupply.filter()
-    aquarium5.addWater()
+//    val aquarium5 = Aquarium(LakeWater())
+//    aquarium5.waterSupply.filter()
+//    aquarium5.addWater()
+
+//    val aquarium6 = Aquarium(TapWater())
+//    addItemTo(aquarium6)
+
+    val cleaner = TapWaterCleaner()
+    val aquarium7 = Aquarium(TapWater())
+//    aquarium7.waterSupply.addChemicalCleaners()
+    aquarium7.addWater(cleaner)
 
 }
+
+
+fun addItemTo(aquarium: Aquarium<WaterSupply>) {
+    println("item adicionado")
+}
+
+
+
+
+
+
+
 
 fun main() {
     genericsExample()
