c374c6339bf92f103de1474790bad213b0daf18a
Dagmar Cirino <dagmar.mv@gmail.com>
2021-07-08 01:11:18 -0300

    lambdas simples

--- src/main/kotlin/example/myapp/example/Annotations.kt
+++ src/main/kotlin/example/myapp/example/Annotations.kt
@@ -1,10 +1,18 @@
 package example.myapp.example
 import kotlin.reflect.full.*    // required import
 
+@Target(AnnotationTarget.PROPERTY_GETTER)
+annotation class OnGet
+
+@Target(AnnotationTarget.PROPERTY_SETTER)
+annotation class OnSet
 
 @ImAPlant class Plant {
-    fun trim() {}
-    fun fertilize() {}
+    @get:OnGet
+    val isGrowing: Boolean = true
+
+    @set:OnSet
+    var needsFood: Boolean = false
 }
 
 fun testAnnotations() {
@@ -20,6 +28,17 @@ fun testAnnotations() {
 
 annotation class ImAPlant
 
+fun labels() {
+    outerLoop@ for (i in 1..100) {
+        print("$i ")
+        for (j in 1..100) {
+            if (i > 10) break@outerLoop  // breaks to outer loop
+        }
+    }
+}
+
+
 fun main() {
-    testAnnotations()
+//    testAnnotations()
+    labels()
 }
